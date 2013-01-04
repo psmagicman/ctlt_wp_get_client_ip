30c89b1c8e042e9ab8e630499ae30c16f9c89461
Devindra Payment <arden.ived@gmail.com>
2013-01-04 14:26:07 -0800

    Update get-client-ip-shortcode.php
    Added license

--- get-client-ip-shortcode.php
+++ get-client-ip-shortcode.php
@@ -9,6 +9,20 @@
  * Author: Michael Ha, Devindra Payment, Julien Law, CTLT
  * Author URI: http://ctlt.ubc.ca/
  */
+/* 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 class IP_Shortcode {
